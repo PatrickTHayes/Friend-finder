a3bca512679ceff426c1232bf19e0509a0491afa
Patrick T Hayes <pat2382@yahoo.com>
2017-12-12 17:07:46 -0500

    initial add of clone

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var htmlRoutes = require('./app/routing/htmlRoutes');
 var apiRoutes=require('./app/routing/apiRoutes.js');
 var data = require('./app/data/friends.js');
-var PORT = 8080;
+var PORT = 3000;
 htmlRoutes(app);
 apiRoutes(app);
 
